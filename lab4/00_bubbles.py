72f3c89ab66db3867a90a2e0c79d6b17c77ccca6
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2022-11-07 18:37:33 +0500

    6

--- lab4/00_bubbles.py
+++ lab4/00_bubbles.py
@@ -19,27 +19,26 @@ def bubble(point, step, col):
                       sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE, sd.COLOR_DARK_BLUE, sd.COLOR_DARK_PURPLE, sd.COLOR_DARK_ORANGE)
     for _ in range(3):
         #radius += step
-        sd.circle(center_position=point, radius=step, color=all_col[col], width=15)
+        sd.circle(center_position=point, radius=step, color=all_col[col], width=2)
 
 
-bubble(point=sd.get_point(300, 300), step=10, col=2)
+#bubble(point=sd.get_point(300, 300), step=10, col=2)
 
 # Нарисовать 10 пузырьков в ряд
 for x in range(100, 1001, 100):
-    bubble(point=sd.get_point(x, 100), step=5, col=3)
+    bubble(point=sd.get_point(x, 40), step=10, col=1)
 
 # Нарисовать три ряда по 10 пузырьков
 # Здесь используется вложенный цикл
 for y in range(100, 301, 100):
     for x in range(100, 1001, 100):
         bubble(point=sd.get_point(x, y), step=10, col=6)
-        #sd.circle(center_position=sd.get_point(x, y), radius=15, color=sd.COLOR_BLACK, width=15)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 
 import random as rnd
 
 for _ in range(100):
-    bubble(point=sd.random_point(), step=5, col=rnd.randint(0,9))
+    bubble(point=sd.random_point(), step=3, col=rnd.randint(0,9))
 
 sd.pause()
